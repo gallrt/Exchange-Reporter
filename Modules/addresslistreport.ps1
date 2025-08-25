5b971700885ebc1fdde4cd049b97f6eb26c8d11c
gallrt <gallrt>
2025-08-25 16:00:22 +0200

    Reformat Code with Code formatter
    Changes are only whitespace, indentation and position of braces (brace position according to One True Brace Style variant of the Kernighan and Ritchie style).
    Done with the VSCode Powershell extension Code formatter.

--- Modules/addresslistreport.ps1
+++ Modules/addresslistreport.ps1
@@ -2,40 +2,35 @@
 
 $addresslistreport = Generate-ReportHeader "addresslistreport.png" "$l_addresslist_header"
 
-$cells=@("$l_addresslist_name","$l_addresslist_hasmember","$l_addresslist_membercount")
+$cells = @("$l_addresslist_name", "$l_addresslist_hasmember", "$l_addresslist_membercount")
 $addresslistreport += Generate-HTMLTable "$l_addresslist_t1header" $cells
 
 $addresslists = Get-Addresslist | sort name
-foreach ($addresslist in $addresslists)
-	{
-		$addresslistname = $addresslist.name
-		$addresslistmembers = Get-Recipient -RecipientPreviewFilter $addresslist.RecipientFilter -OrganizationalUnit $addresslist.RecipientContainer -resultsize unlimited | sort name
-		if ($addresslistmembers)
-			{
-				$addresslisthasmember = "$l_addresslist_memberyes"
-				$addresslistmemcount = $addresslistmembers.count
-				
-				$memcells=@("Name","Typ")
-				$addresslistmemberreport += Generate-HTMLTable "$l_addresslist_t2header $addresslistname" $memcells
-				
-				foreach ($addresslistmember in $addresslistmembers)
-					{
-						$memname = $addresslistmember.name
-						$memtyp = $addresslistmember.RecipientType
-						
-						$memcells=@("$memname","$memtyp")
-						$addresslistmemberreport += New-HTMLTableLine $memcells
-					}
-				$addresslistmemberreport += End-HTMLTable
-			}
-		else
-			{
-				$addresslisthasmember = "$l_addresslist_memberno"
-				$addresslistmemcount = "0"
-			}
-		$cells=@("$addresslistname","$addresslisthasmember","$addresslistmemcount")
-		$addresslistreport += New-HTMLTableLine $cells
+foreach ($addresslist in $addresslists) {
+	$addresslistname = $addresslist.name
+	$addresslistmembers = Get-Recipient -RecipientPreviewFilter $addresslist.RecipientFilter -OrganizationalUnit $addresslist.RecipientContainer -resultsize unlimited | sort name
+	if ($addresslistmembers) {
+		$addresslisthasmember = "$l_addresslist_memberyes"
+		$addresslistmemcount = $addresslistmembers.count
+
+		$memcells = @("Name", "Typ")
+		$addresslistmemberreport += Generate-HTMLTable "$l_addresslist_t2header $addresslistname" $memcells
+
+		foreach ($addresslistmember in $addresslistmembers) {
+			$memname = $addresslistmember.name
+			$memtyp = $addresslistmember.RecipientType
+
+			$memcells = @("$memname", "$memtyp")
+			$addresslistmemberreport += New-HTMLTableLine $memcells
+		}
+		$addresslistmemberreport += End-HTMLTable
+	} else {
+		$addresslisthasmember = "$l_addresslist_memberno"
+		$addresslistmemcount = "0"
 	}
+	$cells = @("$addresslistname", "$addresslisthasmember", "$addresslistmemcount")
+	$addresslistreport += New-HTMLTableLine $cells
+}
 
 $addresslistreport += End-HTMLTable
 
